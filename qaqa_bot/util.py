824b3d290fde80c6f060dc4529310872e2a56e56
Michael Thies <mail@mhthies.de>
2020-04-19 17:55:36 +0200

    util: Add function for programmatic alembic upgrade

--- qaqa_bot/util.py
+++ qaqa_bot/util.py
@@ -1,5 +1,9 @@
+import os.path
 from contextlib import contextmanager
 
+import alembic
+import alembic.config
+import alembic.script
 import sqlalchemy.orm
 
 
@@ -16,3 +20,36 @@ def session_scope(Session: sqlalchemy.orm.sessionmaker):
         raise
     finally:
         session.close()
+
+
+def run_migrations(engine: sqlalchemy.engine.Engine):
+    """
+    Programmatically run `alembic upgrade head`.
+
+    This method creates a simple alembic Configuration, pointing to our versions directory in ../alembic/versions,
+    creates the alembic environment context objects, configures it the
+    :class:`~alembic.runtime.migration.MigrationContext` to run migrations for the upgrade function.
+    """
+    # Create alembic Config
+    config = alembic.config.Config()
+    # `script_location` is not really required for our use case, but alembic refuses to construct the `ScriptDirectory`
+    # without it.
+    config.set_main_option('script_location', os.path.join(os.path.dirname(__file__), '..', 'alembic'))
+    # multiple locations comma-separated
+    config.set_main_option('version_locations', os.path.join(os.path.dirname(__file__), '..', 'alembic', 'versions'))
+    # Create ScriptDirectory and EnvironmentContext objects
+    script_directory = alembic.script.ScriptDirectory.from_config(config)
+    context = alembic.environment.EnvironmentContext(config, script_directory)
+
+    # Function to run migrations for (upgrade to head)
+    def do_upgrade(revision, _context):
+        return script_directory._upgrade_revs('head', revision)
+
+    # Run migrations
+    with engine.connect() as connection:
+        context.configure(
+            connection=connection, fn=do_upgrade
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
